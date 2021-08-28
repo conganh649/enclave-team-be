392380757019659466f9ad81ac5e46ab11c89af5
conganh649 <tranconganh1999@gmail.com>
2021-08-28 10:06:17 +0700

    Test deploy CircleCI

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const connectDB = require("./connection/connection");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 
+console.log(process.env.DB_URL);
 //Models
 const { User } = require("./models/user");
 
